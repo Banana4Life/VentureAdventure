2e31b89c16a4bf5118c19eb1c1f33f99c737a471
Phillip Schichtel <phillip.public@schich.tel>
2017-01-28 14:22:31 +0100

    Initial graph

---
+++ Assets/Scripts/NodeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class NodeController : MonoBehaviour
+{
+    [SerializeField]
+    public Node node;
+
+    [SerializeField]
+    public List<NodeController> neighbors = new List<NodeController>();
+
+
+}
