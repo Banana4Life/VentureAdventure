4bebcd99c5b4a8bddd1ac2a2578b1ee38b2c13ac
Phillip Schichtel <phillip.public@schich.tel>
2017-01-28 19:17:41 +0100

    random walking

---
+++ Assets/Scripts/World/TestNodeMover.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Model;
+using UnityEngine;
+using Util;
+
+namespace World
+{
+    public class TestNodeMover : MonoBehaviour
+    {
+        public WorldGraphController graph;
+        private NodeController previousNode;
+        public NodeController currentNode;
+        private NodeController nextNode;
+
+        private void Update()
+        {
+            if (nextNode == null)
+            {
+                var possibleNext = graph.GetNeighborsOf(currentNode).Where(n => n != previousNode).ToList();
+                if (possibleNext.Count == 0)
+                {
+                    nextNode = previousNode;
+                }
+                else
+                {
+                    nextNode = possibleNext.Random();
+                }
+                gameObject.AddComponent<MoveTo>().Move(nextNode.gameObject.transform.position, 0.3f);
+            }
+        }
+
+        void OnMovementCompleted()
+        {
+            previousNode = currentNode;
+            currentNode = nextNode;
+            nextNode = null;
+        }
+    }
+}
