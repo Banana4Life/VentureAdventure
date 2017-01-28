bd8dcbeb0479a5f40291c02620fbf19bd87bcf20
Phillip Schichtel <phillip.public@schich.tel>
2017-01-28 17:57:26 +0100

    mover component

---
+++ Assets/Scripts/Util/MoveTo.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace Util
+{
+    public class MoveTo : MonoBehaviour
+    {
+        private bool start = false;
+        private Vector3 targetPosition;
+        private Vector3 sourcePosition;
+        public float speed = 1;
+
+        private void Update()
+        {
+            if (start)
+            {
+                if (sourcePosition == Vector3.zero)
+                {
+                    sourcePosition = transform.position;
+                }
+
+                transform.position = Vector3.Lerp(sourcePosition, targetPosition, speed * Time.deltaTime);
+                if (transform.position == targetPosition)
+                {
+                    Destroy(this);
+                }
+            }
+        }
+
+        public void Move(Vector3 target)
+        {
+            sourcePosition = transform.position;
+            targetPosition = target;
+            start = true;
+        }
+
+        public static void MoveObject(GameObject go, Vector3 target, float speed)
+        {
+            var mover = go.AddComponent<MoveTo>();
+            mover.speed = speed;
+            mover.Move(target);
+        }
+    }
+}
