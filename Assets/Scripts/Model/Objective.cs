7cdb24548512f3f45fa24a57876e697eb55e0e67
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-01-29 14:08:40 +0100

    Changed Scene canvas to world space.

--- Assets/Scripts/Model/Objective.cs
+++ Assets/Scripts/Model/Objective.cs
@@ -7,5 +7,6 @@ namespace Model
         public ObjectiveType ObjectiveType { get; set; }
         public int GoldReward { get; set; }
         public Node Node { get; set; }
+        public bool IsClaimed { get; set; }
     }
 }
