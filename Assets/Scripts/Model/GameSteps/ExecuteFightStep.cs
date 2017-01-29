8b03c909e55f97ff98cb84ebacf61cdfaa27f05c
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-01-29 17:36:09 +0100

    Fixed fight step.

--- Assets/Scripts/Model/GameSteps/ExecuteFightStep.cs
+++ Assets/Scripts/Model/GameSteps/ExecuteFightStep.cs
@@ -15,19 +15,18 @@ namespace Model.GameSteps
             var heroParty = State.HeroParty;
             var monsterParty = State.Monsters.FirstOrDefault(party => party.CurrentNode == heroParty.CurrentNode);
 
-            if (monsterParty != null)
+            if (monsterParty != null && monsterParty.Any(u => u.IsAlive))
             {
                 State.BatteRunning = true;
                 Battle.Run(heroParty, monsterParty, monsterParty.IsHidden);
-            }
 
-            
-            yield return new WaitForSeconds(1f);
+                yield return new WaitForSeconds(1f);
 
-            State.BatteRunning = false;
-            if (!State.HeroParty.Any(h => h.IsAlive))
-            {
-                State.RoundFinished = true;
+                State.BatteRunning = false;
+                if (!State.HeroParty.Any(h => h.IsAlive))
+                {
+                    State.RoundFinished = true;
+                }
             }
 
             Complete = true;
