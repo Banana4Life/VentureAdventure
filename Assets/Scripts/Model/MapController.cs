fabfbd5679ba871fa36819d358d02483f42283f0
Rodewald <anna.sophie.rodewald@sap.com>
2017-01-29 15:15:46 +0100

    added MapMusic

---
+++ Assets/Scripts/Model/MapController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Model
+{
+   public class MapController : MonoBehaviour
+    {
+        public void Awake()
+        {
+          //  SceneManager.LoadScene("Map", LoadSceneMode.Additive);
+        }
+
+        public void ShowMap()
+        {
+            //var rootGos = SceneManager.GetSceneByName("Map").GetRootGameObjects();
+            //foreach (var go in rootGos)
+            //{
+            //  //  if (go.name == "GameRoot")
+            //  //      go.SetActive(true);
+            //    //else 
+            //    if (go.name == "MapMusic")
+            //    {
+            //        go.GetComponent<AudioSource>().mute = false;
+            //    }
+            //}
+            GameObject.Find("MapMusic").GetComponent<AudioSource>().mute = false;
+        }
+
+        public void HideMap()
+        {
+            //GameObject.Find("GameRoot").SetActive(false);
+            GameObject.Find("MapMusic").GetComponent<AudioSource>().mute = true;
+        }
+    }
+}
