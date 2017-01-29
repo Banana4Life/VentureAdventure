cd27e26e56b1cddae48b51c3d681cb244f9cf018
Rodewald <anna.sophie.rodewald@sap.com>
2017-01-29 13:12:31 +0100

    updated ObjectiveController and Objective and updated Map.unity and ObjectivePrefab

--- Assets/Scripts/Model/Objective.cs
+++ Assets/Scripts/Model/Objective.cs
@@ -1,8 +1,11 @@
+using Model.World;
+
 namespace Model
 {
     public class Objective
     {
         public ObjectiveType ObjectiveType { get; set; }
         public int GoldReward { get; set; }
+        public Node Node { get; set; }
     }
 }
