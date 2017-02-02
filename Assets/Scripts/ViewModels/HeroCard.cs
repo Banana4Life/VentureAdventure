b828f1ddabb258589f2d9a53f5086d26430defe7
Ruben Weituschewitz <weituschewitz@gmail.com>
2017-02-02 17:21:48 +0100

    Cleanup of HeroCard View

--- Assets/Scripts/ViewModels/HeroCard.cs
+++ Assets/Scripts/ViewModels/HeroCard.cs
@@ -9,6 +9,7 @@ namespace ViewModels
         public _string Name;
         public _int Level;
         public _string HeroClass;
+        public _Sprite HeroSprite;
         public _int ProfitCut;
         public _int Investment;
     }
